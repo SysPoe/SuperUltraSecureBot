7e1187425b21afa287389c0192c14c0dca51ef63
SysPoe <panddplayz@outlook.com>
2021-08-17 15:55:36 +1000

    Updated bot. Added audit log.

--- Bot/src/Config.java
+++ Bot/src/Config.java
@@ -3,10 +3,14 @@ public class Config {
     public String[] blockedWords;
     public String[] bypass;
     public String[] novoicechat;
-    public Config(String[] muted, String[] blockedWords, String[] bypass, String[] novoicechat) {
+    public long auditLogChannelId;
+    public long auditLogServerId;
+    public Config(String[] muted, String[] blockedWords, String[] bypass, String[] novoicechat, long auditLogChannelId, long auditLogServerId) {
         this.muted = muted;
         this.blockedWords = blockedWords;
         this.bypass = bypass;
         this.novoicechat = novoicechat;
+        this.auditLogChannelId = auditLogChannelId;
+        this.auditLogServerId = auditLogServerId;
     }
 }
--- Bot/src/Main.java
+++ Bot/src/Main.java
@@ -1,22 +1,28 @@
 import com.google.gson.Gson;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
-import org.javacord.api.entity.permission.PermissionType;
+import org.javacord.api.entity.activity.Activity;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.permission.*;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.interaction.*;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
     static DiscordApi api;
     static Config config;
-    static final long serverId = 875516599417831505L;
+    static final long serverId = 821261855753371649L;
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         api = new DiscordApiBuilder()
                 .setToken(Token.token)
@@ -26,13 +32,98 @@ public class Main {
         loadConfig();
         System.out.println("Loading listeners...");
         loadListeners();
+        loadAuditLogListeners();
         System.out.println("Loaded listeners.");
         // System.out.println("Adding shutdown listener...");
         // addShutdownListener();
         // System.out.println("Added shutdown listener.");
-        System.out.println("Registering slash commands...");
-        registerSlashCommands();
-        System.out.println("Registered slash commands.");
+        // System.out.println("Registering slash commands...");
+        // registerSlashCommands();
+        // System.out.println("Registered slash commands.");
+    }
+
+    private static void loadAuditLogListeners() {
+        // Messages
+        api.addMessageDeleteListener(messageDeleteEvent -> {
+           if(messageDeleteEvent.getChannel().asTextChannel().get() != api.getServerById(config.auditLogServerId).get().getTextChannelById(config.auditLogChannelId).get()) messageDeleted(messageDeleteEvent.getMessageAuthor().get().getId(), messageDeleteEvent.getChannel().getId(), messageDeleteEvent.getMessage().get().getContent());
+        });
+        api.addMessageEditListener(messageEditEvent -> {
+            messageEdited(messageEditEvent.getMessageAuthor().get().getId(), messageEditEvent.getChannel().getId(), messageEditEvent.getOldContent().get(), messageEditEvent.getNewContent());
+        });
+
+        // Channels
+        api.addServerChannelDeleteListener(serverChannelDeleteEvent -> {
+            serverChannelCreatedOrDeleted(serverChannelDeleteEvent.getChannel().getName(), "deleted");
+        });
+        api.addServerChannelCreateListener(serverChannelCreateEvent -> {
+            serverChannelCreatedOrDeleted(serverChannelCreateEvent.getChannel().getName(), "creted");
+        });
+        api.addServerChannelChangeOverwrittenPermissionsListener(serverChannelChangeOverwrittenPermissionsEvent ->   {
+            serverOverridePermsChange(serverChannelChangeOverwrittenPermissionsEvent.getChannel().getId(), getChangedPerms(serverChannelChangeOverwrittenPermissionsEvent.getOldPermissions(), serverChannelChangeOverwrittenPermissionsEvent.getNewPermissions()));
+        });
+
+        // Text Channels
+        api.addServerChannelChangeNameListener(serverChannelChangeNameEvent -> {
+            serverChannelValueChange(serverChannelChangeNameEvent.getChannel().getId(), "name", serverChannelChangeNameEvent.getOldName(), serverChannelChangeNameEvent.getNewName());
+        });
+        api.addServerChannelChangeNsfwFlagListener(serverChannelChangeNsfwFlagEvent -> {
+            serverChannelValueChange(serverChannelChangeNsfwFlagEvent.getChannel().getId(), "NSFW Flag", String.valueOf(serverChannelChangeNsfwFlagEvent.getOldNsfwFlag()), String.valueOf(serverChannelChangeNsfwFlagEvent.getNewNsfwFlag()));
+        });
+        api.addServerTextChannelChangeSlowmodeListener(serverTextChannelChangeSlowmodeEvent -> {
+            serverChannelValueChange(serverTextChannelChangeSlowmodeEvent.getChannel().getId(), "slow mode", serverTextChannelChangeSlowmodeEvent.getOldDelayInSeconds() + " seconds", serverTextChannelChangeSlowmodeEvent.getNewDelayInSeconds() + " seconds");
+        });
+        api.addServerTextChannelChangeTopicListener(serverTextChannelChangeTopicEvent -> {
+            serverChannelValueChange(serverTextChannelChangeTopicEvent.getChannel().getId(), "topic", serverTextChannelChangeTopicEvent.getOldTopic(), serverTextChannelChangeTopicEvent.getNewTopic());
+        });
+
+        // Voice Channels
+        api.addServerVoiceChannelChangeBitrateListener(serverVoiceChannelChangeBitrateEvent -> {
+            serverChannelValueChange(serverVoiceChannelChangeBitrateEvent.getChannel().getId(), "bitrate", serverVoiceChannelChangeBitrateEvent.getOldBitrate()+"kbps", serverVoiceChannelChangeBitrateEvent.getNewBitrate()+"kbps");
+        });
+        api.addServerVoiceChannelChangeUserLimitListener(serverVoiceChannelChangeUserLimitEvent -> {
+            serverChannelValueChange(serverVoiceChannelChangeUserLimitEvent.getChannel().getId(), "user limit", serverVoiceChannelChangeUserLimitEvent.getOldUserLimit() + " users", serverVoiceChannelChangeUserLimitEvent.getNewUserLimit() + " users");
+        });
+
+        // Invites
+        api.addServerChannelInviteCreateListener(serverChannelInviteCreateEvent -> {
+            inviteCreated(serverChannelInviteCreateEvent.getChannel().getId(), serverChannelInviteCreateEvent.getInvite().getInviter().get().getId(), serverChannelInviteCreateEvent.getInvite().getUrl().toString());
+        });
+
+        // Members
+        api.addUserChangeActivityListener(userChangeActivityEvent -> {
+            userActivityChanged(userChangeActivityEvent.getUserId(), userChangeActivityEvent.getOldActivities().stream().toList(), userChangeActivityEvent.getNewActivities().stream().toList());
+        });
+        api.addUserRoleAddListener(userRoleAddEvent -> {
+            userRoleAdd(userRoleAddEvent.getUser().getId(), userRoleAddEvent.getRole());
+        });
+    }
+
+    private static String getChangedPerms(Permissions oldPermissions, Permissions newPermissions) {
+        List<PermissionType> oldAllowedPerms = oldPermissions.getAllowedPermission().stream().toList();
+        List<PermissionType> newAllowedPerms = newPermissions.getAllowedPermission().stream().toList();
+        List<PermissionType> oldUnsetPerms = oldPermissions.getUnsetPermissions().stream().toList();
+        List<PermissionType> newUnsetPerms = newPermissions.getUnsetPermissions().stream().toList();
+        List<PermissionType> oldDeniedPerms = oldPermissions.getDeniedPermissions().stream().toList();
+        List<PermissionType> newDeniedPerms = newPermissions.getDeniedPermissions().stream().toList();
+
+        AtomicReference<String> changes = new AtomicReference<>("");
+        oldAllowedPerms.forEach(permissionType -> {
+            if(!newAllowedPerms.contains(permissionType)) {
+                if(newUnsetPerms.contains(permissionType)) changes.set(changes.get()+":yellow_square:"+permissionType.name()+"\n");
+            }
+        });
+        newAllowedPerms.forEach(permissionType -> {
+            if(!oldAllowedPerms.contains(permissionType)) {
+                changes.set(changes.get()+":green_square:"+permissionType.name()+"\n");
+            }
+        });
+        oldDeniedPerms.forEach(permissionType -> {
+            if(newUnsetPerms.contains(permissionType)) changes.set(changes.get()+":yellow_square:"+permissionType.name()+"\n");
+        });
+        newDeniedPerms.forEach(permissionType -> {
+            if(oldAllowedPerms.contains(permissionType) || oldUnsetPerms.contains(permissionType)) changes.set(changes.get()+":red_square:"+permissionType.name()+"\n");
+        });
+        return changes.get();
     }
     private static void addShutdownListener() {
         // Runtime.getRuntime().addShutdownHook(new Thread(Main::save));
@@ -147,7 +238,7 @@ public class Main {
                         long id = slashCommandInteraction.getUser().getId();
                             if (userHasAdministrator(id) || userIsBypassing(id)) {
                                 slashCommandInteraction.getChannel().get().getMessages(slashCommandInteraction.getFirstOptionIntValue().get()).get().deleteAll().join();
-                                slashCommandInteraction.createImmediateResponder().setContent("Channel cleared").respond();
+                                slashCommandInteraction.createImmediateResponder().setContent("Cleared " + slashCommandInteraction.getFirstOptionIntValue().get() + " messages.").respond();
                             }
                             else slashCommandInteraction.createImmediateResponder().setContent("You do not have permission to do that!").respond();
                     } catch (Exception e) {
@@ -403,6 +494,122 @@ public class Main {
             config = new Gson().fromJson(cfg.toString(), Config.class);
             System.out.println("Config loaded.");
         } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void messageDeleted(long authorId, long channelId, String content) {
+        try {
+            TextChannel channel = api.getServerById(config.auditLogServerId).get().getTextChannelById(config.auditLogChannelId).get();
+            channel.sendMessage(new EmbedBuilder()
+                    .setAuthor(api.getUserById(authorId).get())
+                    .setColor(Color.RED)
+                    .setTitle("Message deleted")
+                    .setDescription("**Message by <@" + authorId + "> deleted in <#" + channelId + ">**\n" + content)
+            ).join();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    private static void messageEdited(long authorId, long channelId, String oldContent, String newContent) {
+        try {
+            TextChannel channel = api.getServerById(config.auditLogServerId).get().getTextChannelById(config.auditLogChannelId).get();
+            channel.sendMessage(new EmbedBuilder()
+                    .setAuthor(api.getUserById(authorId).get())
+                    .setColor(Color.RED)
+                    .setTitle("Message edited")
+                    .setDescription("**Message by <@"+authorId+"> edited in <#"+channelId+">**\n" +
+                            "**Old content:** "+oldContent+"\n" +
+                            "**New content:** "+newContent)
+            ).join();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    private static void serverChannelCreatedOrDeleted(String channelName, String actionType) {
+        TextChannel channel = api.getServerById(config.auditLogServerId).get().getTextChannelById(config.auditLogChannelId).get();
+        channel.sendMessage(new EmbedBuilder()
+                .setAuthor(api.getYourself())
+                .setColor(Color.RED)
+                .setTitle("Channel "+actionType)
+                .setDescription("Channel #"+channelName+" "+actionType+".")
+        ).join();
+    }
+    private static void serverChannelValueChange(long channelId, String actionType, String oldValue, String newValue) {
+        try {
+            TextChannel channel = api.getServerById(config.auditLogServerId).get().getTextChannelById(config.auditLogChannelId).get();
+            channel.sendMessage(new EmbedBuilder()
+                    .setAuthor(api.getYourself())
+                    .setColor(Color.RED)
+                    .setTitle("Channel "+actionType+" edited")
+                    .setDescription("**<#"+channelId+">'s "+actionType+" has been changed!\n" +
+                            "**Old value: **"+oldValue+"\n" +
+                            "**New value: **"+newValue)
+            ).join();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    private static void serverOverridePermsChange(long channelId, String changes) {
+        TextChannel channel = api.getServerById(config.auditLogServerId).get().getTextChannelById(config.auditLogChannelId).get();
+        channel.sendMessage(new EmbedBuilder()
+                .setAuthor(api.getYourself())
+                .setColor(Color.RED)
+                .setTitle("<#"+channelId+">'s override permissions have been edited")
+                .setDescription("**Changes: **"+changes+"\n")
+        ).join();
+    }
+    private static void inviteCreated(long channelId, long inviterId, String url) {
+        try {
+            TextChannel channel = api.getServerById(config.auditLogServerId).get().getTextChannelById(config.auditLogChannelId).get();
+            channel.sendMessage(new EmbedBuilder()
+                    .setAuthor(api.getUserById(inviterId).get())
+                    .setColor(Color.RED)
+                    .setTitle("Invite created")
+                    .setDescription("**<@" + inviterId+ "> created invite for <#" + channelId + ">!**\n"+url)
+                    .setUrl(url)
+            ).join();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    private static void userActivityChanged(long userId, List<Activity> oldActivity, List<Activity> newActivity) {
+        try {
+            TextChannel channel = api.getServerById(config.auditLogServerId).get().getTextChannelById(config.auditLogChannelId).get();
+            AtomicReference<String> oldActivities = new AtomicReference<>("");
+            AtomicReference<String> newActivites = new AtomicReference<>("");
+            oldActivity.forEach(activity -> {oldActivities.set(", " + oldActivities.get() + activity);});
+            newActivity.forEach(activity -> {newActivites.set(", " + newActivites.get() + activity);});
+            oldActivities.set(oldActivities.get().replaceFirst(", ", ""));
+            newActivites.set(newActivites.get().replaceFirst(", ", ""));
+            channel.sendMessage(new EmbedBuilder()
+                    .setAuthor(api.getUserById(userId).get())
+                    .setColor(Color.RED)
+                    .setTitle("User activity changed")
+                    .setDescription("**<@"+userId+">'s activities has been changed.**\nPrevious activities: "+oldActivities.get()+"\nCurrent activities"+newActivites.get())
+            ).join();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    private static void userRoleAdd(long userId, Role role) {
+        System.out.println(userId);
+        System.out.println(new Gson().toJson(role));
+        try {
+            TextChannel channel = api.getServerById(config.auditLogServerId).get().getTextChannelById(config.auditLogChannelId).get();
+            channel.sendMessage(new EmbedBuilder()
+                    .setAuthor(api.getUserById(userId).get())
+                    .setColor(Color.RED)
+                    .setTitle("<@"+userId+">'s roles have been changed!")
+                    .setDescription("**Added role: "+role.getMentionTag()+"**")
+            ).join();
+        } catch (Exception e) {
             System.err.println(e.getMessage());
             e.printStackTrace();
         }
